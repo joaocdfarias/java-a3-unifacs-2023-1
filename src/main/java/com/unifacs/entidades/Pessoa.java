f48badf85b1cba903caed154ed7f98b3d299375b
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2023-05-24 22:06:24 -0300

    feat: fix

--- src/main/java/com/unifacs/entidades/Pessoa.java
+++ src/main/java/com/unifacs/entidades/Pessoa.java
@@ -4,34 +4,20 @@ public abstract class Pessoa {
     private String nome;
     private String cpf;
 
-    public Pessoa(){
-
+    public Pessoa() {
     }
-    public Pessoa(String nome, String cpf){
+
+    public Pessoa(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
-
     }
 
-
-
-    /**
-     * @return String return the nome
-     */
     public String getNome() {
         return nome;
     }
 
-
-
-    /**
-     * @return String return the cpf
-     */
     public String getCpf() {
         return cpf;
     }
 
-
-
 }
-
