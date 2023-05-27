23e1530287ae9390f42ddc3f23aa8d097fd59165
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2023-05-27 19:48:00 -0300

    style: format code

--- src/main/java/com/unifacs/entidades/Aluno.java
+++ src/main/java/com/unifacs/entidades/Aluno.java
@@ -9,55 +9,56 @@ public class Aluno extends Pessoa {
     private float media;
     private ArrayList<Float> notas = new ArrayList<>();
 
-    public Aluno(){
+    public Aluno() {
         super();
     }
-    
-    public Aluno(String nome, String cpf, String periodo, String ra, String curso){
+
+    public Aluno(String nome, String cpf, String periodo, String ra, String curso) {
         super(nome, cpf);
         this.periodo = periodo;
         this.ra = ra;
         this.curso = curso;
     }
 
-    //Metodo que informa as notas do aluno
-    public String acessarNotas(){
+    // Metodo que informa as notas do aluno
+    public String acessarNotas() {
         StringBuilder x = new StringBuilder();
-        for(int i =0;i<notas.size(); i++){
-            x.append(String.format("%.2f", notas.get(i))+ " ");
+        for (int i = 0; i < notas.size(); i++) {
+            x.append(String.format("%.2f", notas.get(i)) + " ");
         }
         return x.toString();
 
     }
-    //Metodo que remove as notas do aluno
-    public void removerNota(int nota){
+
+    // Metodo que remove as notas do aluno
+    public void removerNota(int nota) {
         notas.remove(nota);
     }
-    
-    //Metodo que adiciona as notas do aluno
-    public void adicionarNota(float nota){
+
+    // Metodo que adiciona as notas do aluno
+    public void adicionarNota(float nota) {
         notas.add(nota);
     }
-    
-    //Metodo que adiciona uma nota no lugar de outra nota do aluno
-    public void adicionarNota(int notaLugar, float nota){
-        notas.add(notaLugar-1, nota);
+
+    // Metodo que adiciona uma nota no lugar de outra nota do aluno
+    public void adicionarNota(int notaLugar, float nota) {
+        notas.add(notaLugar - 1, nota);
     }
 
-    //Metodo que calcula a media do aluno
-    public float calcularMedia(){
+    // Metodo que calcula a media do aluno
+    public float calcularMedia() {
         media = 0f;
 
-        for(float x : notas){
-            media+=x;
+        for (float x : notas) {
+            media += x;
         }
-        media = media/notas.size();
+        media = media / notas.size();
         return media;
     }
-    
-    //Metodo que que retorna uma String mostrando 3 atributos do aluno
-    public String toString(){
-        return "Nome: "+super.getNome()+ " - RA:"+ ra + " - Curso: "+curso;
+
+    // Metodo que que retorna uma String mostrando 3 atributos do aluno
+    public String toString() {
+        return "Nome: " + super.getNome() + " - RA:" + ra + " - Curso: " + curso;
     }
 
     /**
@@ -81,7 +82,6 @@ public class Aluno extends Pessoa {
         return ra;
     }
 
-    
     /**
      * @return String return the periodo
      */
@@ -102,5 +102,5 @@ public class Aluno extends Pessoa {
     public ArrayList<Float> getNotas() {
         return notas;
     }
-    
+
 }
